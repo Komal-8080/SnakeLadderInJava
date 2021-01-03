d8b4cb575aabdc3dee71dedb4449b15a5b4a150a
Komal-8080 <shindeg163@gmail.com>
2021-01-03 17:37:31 +0530

    Added: Random to roll the die and get the number between 1 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,14 +1,22 @@
 package snakeLadder;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
 	
 	int startPosition = 0;
 	int numberofPlayers = 1;
 	
+	public static int playerRollsTheDie() {
+		Random numberOnDie = new Random();
+		return numberOnDie.nextInt(6)+1;		
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("****Let's play Snake and Ladder Game*****");
-		
+		System.out.println("Number on dice is: " + playerRollsTheDie());
+	
 	}
 
 }
