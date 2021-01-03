6876c31575653bee38253ded143e1f598b9b3ec3
Komal-8080 <shindeg163@gmail.com>
2021-01-03 17:31:15 +0530

    Added: Single player at start position 0

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,6 +2,9 @@ package snakeLadder;
 
 public class SnakeAndLadder {
 	
+	int startPosition = 0;
+	int numberofPlayers = 1;
+	
 	public static void main(String[] args) {
 		
 		System.out.println("****Let's play Snake and Ladder Game*****");
