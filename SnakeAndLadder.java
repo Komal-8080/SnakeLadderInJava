e389627427e87a513fffaba0eeff8d09aef57969
Komal-8080 <shindeg163@gmail.com>
2021-01-03 17:27:24 +0530

    Initialization: Snake and Ladder Game initialized with main method

---
+++ SnakeAndLadder.java
@@ -0,0 +1,11 @@
+package snakeLadder;
+
+public class SnakeAndLadder {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("****Let's play Snake and Ladder Game*****");
+		
+	}
+
+}
